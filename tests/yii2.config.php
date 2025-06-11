ef295ff01f283ffb563dddcc9123e63de933f622
Joey Claessen <joey.claessen@gmail.com>
2025-06-11 16:36:14 +0200

    fix: fixed tests

--- tests/yii2.config.php
+++ tests/yii2.config.php
@@ -5,6 +5,14 @@ return [
     'id' => 'yii2-phpfpm-test',
     'basePath' => __DIR__ . '/../src',
     'components' => [
+        'i18n' => [
+            'translations' => [
+                'sam-it.urlsigner' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'fileMap' => [],
+                ],
+            ],
+        ],
         'urlSigner' => SamIT\Yii2\UrlSigner\UrlSigner::class
     ]
 ];
